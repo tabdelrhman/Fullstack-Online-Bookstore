864679fb695506704e6a434910cd0dcac39f35c7
abdtahmed <abdtahmed.mail>
2021-01-17 14:47:42 +0300

    correct commit2

---
+++ onlineBookStore-Angular/src/app/common/book.ts
@@ -0,0 +1,13 @@
+export class Book {
+
+    sku: string;
+    name: string;
+    description: string;
+    unit_price: number;
+    imageUrl: string;
+    active: boolean;
+    unitInStock: number;
+    createdOn: Date
+    updatedOn: Date;
+    
+}
---
+++ onlineBookStore-Angular/src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,29 @@
+import { BookService } from './../../services/book.service';
+import { Book } from './../../common/book';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+
+  
+  books: Book[];
+
+  constructor(private bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.getListBooks();
+  }
+
+  getListBooks(){
+    this.bookService.getBooks().subscribe(
+      data => {
+        this.books = data;
+          }
+    )
+  }
+
+}
