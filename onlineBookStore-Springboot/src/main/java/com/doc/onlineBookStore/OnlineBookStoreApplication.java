c1bb05b5875262f109d6f4a7c1ccbb2a7dbf8424
abdtahmed <abdtahmed.mail>
2021-03-18 16:50:57 +0300

    Revert "test commit5"
    This reverts commit 7681e604138074cdae029b3e92392533ee316759.

--- onlineBookStore-Springboot/src/main/java/com/doc/onlineBookStore/OnlineBookStoreApplication.java
+++ onlineBookStore-Springboot/src/main/java/com/doc/onlineBookStore/OnlineBookStoreApplication.java
@@ -10,7 +10,6 @@ public class OnlineBookStoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineBookStoreApplication.class, args);
 		System.out.println();
-		System.out.println("finish");
 	}
 
 }
