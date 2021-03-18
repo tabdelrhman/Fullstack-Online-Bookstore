5f03ce0af38b4a2e865c28163011a700cb0c2bd4
abdtahmed <abdtahmed.mail>
2021-03-18 15:15:12 +0300

    Revert "test commit2"
    This reverts commit 199aa2012adc45bd753e430e208c7e7787fad6e0.

--- onlineBookStore-Springboot/src/main/java/com/doc/onlineBookStore/OnlineBookStoreApplication.java
+++ onlineBookStore-Springboot/src/main/java/com/doc/onlineBookStore/OnlineBookStoreApplication.java
@@ -9,7 +9,6 @@ public class OnlineBookStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineBookStoreApplication.class, args);
-		System.out.println();
 	}
 
 }
